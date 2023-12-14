ea1f23ae6557f823c8a56ab0a2a9a09f442039a5
dmitriy-utkin <dm.utkin@icloud.com>
2023-12-14 23:28:15 +0400

    fifth practice initial commit

---
+++ spring-fifth-practice-books/src/test/java/com/example/spring/fifth/SpringFifthPracticeBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.fifth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFifthPracticeBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
